cf74c7b70bd1a01b2bec2ebe6b0dc6fb3df67db9
Michael Bianco <iloveitaly@gmail.com>
2023-01-19 07:48:33 -0700

    feat(snowflake): filter tables, transform sql

--- bin/snowflake-describe.ts
+++ bin/snowflake-describe.ts
@@ -1,37 +1,59 @@
 #!/usr/bin/env node
 // https://github.com/remix-run/remix/blob/main/packages/remix-dev/env.ts
-import { Snowflake, loadFiles } from "snowflake-multisql";
-import invariant from "tiny-invariant";
+import { Command } from "commander"
+import type { LogLevelNames } from "loglevel"
+import log from "loglevel"
+import loglevelPrefix from "loglevel-plugin-prefix"
+import { Snowflake, loadFiles } from "snowflake-multisql"
+import invariant from "tiny-invariant"
 
-import { loadEnv } from "@remix-run/dev/dist/env";
+import { loadEnv } from "@remix-run/dev/dist/env"
+
+function configureLogging() {
+  // TODO extract out to separate logging config
+  // add expected log level prefixes
+  loglevelPrefix.reg(log)
+  log.enableAll()
+  loglevelPrefix.apply(log)
+
+  if (process.env.LOG_LEVEL) {
+    const logLevelFromEnv = process.env.LOG_LEVEL.toLowerCase() as LogLevelNames
+    log.setLevel(logLevelFromEnv)
+  } else {
+    log.setLevel(log.levels.INFO)
+  }
+}
 
 // I checked underscore, rambda, and lodash and none has something simple like `fetch` in ruby :/
 function fetch(ob: any, key: string) {
   if (key in ob) {
-    return ob[key];
+    return ob[key]
   }
 
-  throw new Error(`Key does not exist: ${key}`);
+  throw new Error(`Key does not exist: ${key}`)
 }
 
 // TODO wrap this up into a nice little config function
 // TODO replace console.dir with custom method with better formatting
-const util = require("util");
+const util = require("util")
 declare global {
-  var dir: InstanceType<typeof Function>;
+  var dir: InstanceType<typeof Function>
+  var toJson: InstanceType<typeof Function>
+  var copyToClipboard: InstanceType<typeof Function>
 }
 // https://nodejs.org/api/util.html
-util.inspect.defaultOptions.maxArrayLength = null;
-util.inspect.defaultOptions.colors = true;
+util.inspect.defaultOptions.maxArrayLength = null
+util.inspect.defaultOptions.colors = true
 // TODO check if globals are accessible in a repl
-global.dir = util.inspect;
+global.dir = (ob: any) => process.stdout.write(util.inspect(ob))
 
 function toJson(obj: any) {
-  const jsonStr = JSON.stringify(obj, undefined, 2);
-  copyToClipboard(jsonStr);
-  console.log(jsonStr);
-  return jsonStr;
+  const jsonStr = JSON.stringify(obj, undefined, 2)
+  copyToClipboard(jsonStr)
+  console.log(jsonStr)
+  return jsonStr
 }
+global.toJson = toJson
 
 // copyToClipboard = (text) => {
 function copyToClipboard(text: string) {
@@ -40,8 +62,9 @@ function copyToClipboard(text: string) {
       LC_CTYPE: "UTF-8",
     },
     input: text,
-  });
+  })
 }
+global.copyToClipboard = copyToClipboard
 
 // https://github.com/nodejs/node/issues/9617
 function isDebugEnabled() {
@@ -49,26 +72,26 @@ function isDebugEnabled() {
   const inspectPassedOverCLI =
     process.execArgv.filter(
       (a) => a.includes("--inspect-brk") || a == "inspect"
-    ).length > 0;
-  return require("inspector").url() !== undefined || inspectPassedOverCLI;
+    ).length > 0
+  return require("inspector").url() !== undefined || inspectPassedOverCLI
 }
 
 function repl(context: any) {
   if (isDebugEnabled()) {
     // TODO `error` does not display any colored text... really?
-    console.error("ERROR: Debug mode enabled, do not use repl!");
-    return;
+    console.error("ERROR: Debug mode enabled, do not use repl!")
+    return
   }
 
-  console.log("Starting REPL...");
+  console.log("Starting REPL...")
 
-  const repl = require("repl");
+  const repl = require("repl")
   const replServer = repl.start({
     prompt: "snowflake> ",
     input: process.stdin,
     output: process.stdout,
     useGlobal: true,
-  });
+  })
 
   // TODO remove all 'const ' from the input to avoid redeclaring variables error
 
@@ -76,13 +99,13 @@ function repl(context: any) {
   replServer.defineCommand("vars", {
     help: "List all local variables",
     action: function () {
-      this.displayPrompt();
-      console.log(Object.keys(this.context));
+      this.displayPrompt()
+      console.log(Object.keys(this.context))
     },
-  });
+  })
 
   for (const key in context) {
-    replServer.context[key] = context[key];
+    replServer.context[key] = context[key]
   }
 }
 
@@ -90,11 +113,11 @@ function repl(context: any) {
 // TODO `with` hack https://stackoverflow.com/questions/2051678/getting-all-variables-in-scope
 function tsRepl(context: any) {
   // https://typestrong.org/ts-node/api/index.html#createRepl
-  const tsNode = require("ts-node");
-  const repl = tsNode.createRepl();
-  const service = tsNode.create({ ...repl.evalAwarePartialHost });
-  repl.setService(service);
-  repl.start();
+  const tsNode = require("ts-node")
+  const repl = tsNode.createRepl()
+  const service = tsNode.create({ ...repl.evalAwarePartialHost })
+  repl.setService(service)
+  repl.start()
 
   // for(const key in context) {
   //   repl.context[key] = context[key];
@@ -103,9 +126,6 @@ function tsRepl(context: any) {
 
 // TODO `snowflake> Uncaught Error [ClientError]: Connection already terminated. Cannot connect again.`
 export async function snowflakeConnection() {
-  process.chdir(__dirname + "/..");
-  await loadEnv("");
-
   const snowflake = new Snowflake({
     account: fetch(process.env, "SNOWFLAKE_ACCOUNT"),
     username: fetch(process.env, "SNOWFLAKE_USER"),
@@ -116,37 +136,37 @@ export async function snowflakeConnection() {
     // schema: fetch(process.env, 'SNOWFLAKE_DATABASE'),
     // TODO unsure why warehouse is even an option, maybe if you have more than one warehouse in your snowflake account?
     // warehouse: "DEMO_WH",
-  });
+  })
 
-  await snowflake.connect();
+  await snowflake.connect()
 
-  return snowflake;
+  return snowflake
 }
 
 // TODO incomplete, dump all schema on an account to determine which one we should filter on
 async function listSchemas(snowflake: Snowflake) {
-  const sqlText = "show schemas;";
+  const sqlText = "show schemas;"
   // tags are SQL variable bindings (terrible name)
   const schemaInAccount = await snowflake.executeAll({
     sqlText,
     tags: [],
     includeResults: true,
     preview: false, // set it true for checking statements before sending to Snowflake.
-  });
+  })
 
   // the entire schema list is returned in a single row
-  console.dir(schemaInAccount[0].data);
+  console.dir(schemaInAccount[0].data)
 
-  repl({ snowflake });
+  repl({ snowflake })
 }
 
 // should be IExecuteAllResult
 function extractSingleRowResult(result: any) {
   if (result.length > 1) {
-    throw new Error("Expected only one row");
+    throw new Error("Expected only one row")
   }
 
-  return result[0];
+  return result[0]
 }
 
 export async function executeSQL<T>(snowflake: Snowflake, sql: string) {
@@ -155,17 +175,17 @@ export async function executeSQL<T>(snowflake: Snowflake, sql: string) {
     tags: [],
     includeResults: true,
     preview: false,
-  });
+  })
 
   // TODO no idea why the return value is a list, and the type seems to indicate it's an object
   if (result.length > 1) {
-    throw new Error("Expected only one return data");
+    throw new Error("Expected only one return data")
   }
 
   // strip out undefined from the return types
-  const resultData = result[0].data;
-  invariant(resultData !== undefined);
-  return resultData;
+  const resultData = result[0].data
+  invariant(resultData !== undefined)
+  return resultData
 }
 
 async function describeTableAsSQL(
@@ -175,7 +195,7 @@ async function describeTableAsSQL(
   const result = await executeSQL<{ [key: string]: string }>(
     snowflake,
     `SELECT GET_DDL('TABLE', '${fullyQualifiedTableName}');`
-  );
+  )
 
   /*
   the resulting data structure is:
@@ -187,16 +207,16 @@ async function describeTableAsSQL(
   */
 
   if (result.length > 1) {
-    throw new Error("Expected only one row");
+    throw new Error("Expected only one row")
   }
 
-  const values = Object.values(result[0]);
+  const values = Object.values(result[0])
 
   if (values.length > 1) {
-    throw new Error("Expected only one value");
+    throw new Error("Expected only one value")
   }
 
-  return values[0];
+  return values[0]
 }
 
 // SELECT columnName FROM TABLE_NAME WHERE XYZ
@@ -207,16 +227,16 @@ interface ColumnMetadata {
 
 // no indexes on a columar datastore
 interface DataSourceColumnDefinition {
-  name: string;
-  type: string;
-  kind: string;
-  "null?": string;
-  default: any;
-  "primary key": string;
-  "unique key": string;
-  check: any;
-  expression: any;
-  comment: any;
+  name: string
+  type: string
+  kind: string
+  "null?": string
+  default: any
+  "primary key": string
+  "unique key": string
+  check: any
+  expression: any
+  comment: any
   // "policy name": any
 }
 
@@ -228,87 +248,159 @@ async function describeTable(
   const result = await executeSQL<DataSourceColumnDefinition>(
     snowflake,
     `DESCRIBE TABLE ${fullyQualifiedTableName};`
-  );
+  )
   // TODO filter out desribes that are not columns
-  debugger;
-  return result;
+  debugger
+  return result
 }
 
 export interface TableDescription {
-  created_on: string;
-  name: string;
-  database_name: string;
-  schema_name: string;
-  kind: string;
-  comment: string;
-  cluster_by: string;
-  rows: number;
-  bytes: number;
-  owner: string;
-  retention_time: string;
-  automatic_clustering: string;
-  change_tracking: string;
-  is_external: string;
+  created_on: string
+  name: string
+  database_name: string
+  schema_name: string
+  kind: string
+  comment: string
+  cluster_by: string
+  rows: number
+  bytes: number
+  owner: string
+  retention_time: string
+  automatic_clustering: string
+  change_tracking: string
+  is_external: string
+}
+
+Array.prototype.last = function () {
+  return this[this.length - 1]
+}
+
+async function shouldIncludeTable(
+  database: Snowflake,
+  fullyQualifiedTableName: string
+): Promise<boolean> {
+  const tableName = fullyQualifiedTableName.split(".").last()
+  if (tableName.startsWith("_AIRBYTE")) {
+    log.debug("excluding table", { table: fullyQualifiedTableName })
+    return false
+  }
+
+  if (tableName.endsWith("_SCD")) {
+    log.debug("excluding table", { table: fullyQualifiedTableName })
+    return false
+  }
+
+  const countResult = await executeSQL<{ ["COUNT"]: number }>(
+    database,
+    `SELECT COUNT(*) as count FROM ${fullyQualifiedTableName}`
+  )
+
+  if (countResult[0].COUNT === 0) {
+    log.debug("excluding empty table", {
+      table: fullyQualifiedTableName,
+    })
+    return false
+  }
+
+  return true
 }
 
 async function describeTables(snowflake: Snowflake, database: string) {
-  const showTablesSql = "SHOW TABLES IN DATABASE " + database;
+  debugger
+  const showTablesSql = "SHOW TABLES IN DATABASE " + database
   const snowflakeTableList = await executeSQL<TableDescription>(
     snowflake,
     showTablesSql
-  );
+  )
+  debugger
 
-  const tableDescribeMap: { [key: string]: string } = {};
+  log.debug("got all tables", { count: snowflakeTableList.length })
+
+  let tableDescribeMap: { [key: string]: string } = {}
 
   for (const tableMeta of snowflakeTableList) {
-    // exclude tables that start with "_AIRBYTE"
-    if (tableMeta.name.startsWith("_AIRBYTE")) {
-      continue;
+    const fullyQualifiedTableName = `${tableMeta.database_name}.${tableMeta.schema_name}.${tableMeta.name}`
+
+    if (
+      (await shouldIncludeTable(snowflake, fullyQualifiedTableName)) === false
+    ) {
+      continue
     }
 
-    const fullyQualifiedTableName = `${tableMeta.database_name}.${tableMeta.schema_name}.${tableMeta.name}`;
-    console.log(`describing table ${fullyQualifiedTableName}`);
+    log.debug(`describing table ${fullyQualifiedTableName}`)
 
     // alternatively, use `await describeTable(snowflake, database)` to return table as a JSON array
-    // tableDescribeMap[tableMeta.name] = await describeTableAsSQL(snowflake, fullyQualifiedTableName)
-    tableDescribeMap[tableMeta.name] = await describeTable(
-      snowflake,
-      fullyQualifiedTableName
-    );
+    tableDescribeMap[tableMeta.name] = cleanSnowflakeSQLDescribe(
+      await describeTableAsSQL(snowflake, fullyQualifiedTableName)
+    )
   }
 
-  return tableDescribeMap;
+  return tableDescribeMap
 }
 
-async function main() {
-  const connection = await snowflakeConnection();
-  const tableMap = await describeTables(connection, "DAYSPRINGPENS");
+function cleanSnowflakeSQLDescribe(sqlDescribe: string) {
+  return (
+    sqlDescribe
+      // uppercase takes more tokens, lowercase everything
+      .toLowerCase()
+      // "create or replace" => "create"
+      .replace("create or replace", "create")
+      // VARCHAR(\d) => TEXT
+      .replace(/varchar\([\d,]+\)/g, "text")
+      // number(38,0) => number
+      .replace(/number\([\d,]+\)/g, "number")
+      // timestamp_tz(9) => timestamp
+      .replace(/timestamp_tz\([\d,]+\)/g, "timestamp")
+      // cluster by (_AIRBYTE_EMITTED_AT) => ""
+      .replace(/cluster by \([^)]+\)/g, "")
+      // \n\t_AIRBYTE_UNIQUE_KEY VARCHAR(32), => "" (remember, everything is lowercase at this point)
+      .replace(/\t\_airbyte\_.*/g, "")
+      // remove duplicate lines
+      .replace(/\n{2,}/gm, "\n")
+  )
+}
+
+async function main(databaseName: string) {
+  await init()
+
+  log.info("collecting all tables for SQL dump")
+  const connection = await snowflakeConnection()
+  const tableMap = await describeTables(connection, "DAYSPRINGPENS")
+
+  const jsonMap = JSON.stringify(tableMap, undefined, 2)
+  log.info("writing table map to snowflake.json")
+  require("fs").writeFileSync("./snowflake_filtered.json", jsonMap)
 
   // TODO dump into DB tables
+
+  repl({ tableMap })
 }
 
 async function snowflakeRepl() {
-  const connection = await snowflakeConnection();
-  repl({ connection, executeSQL });
+  await init()
+
+  const connection = await snowflakeConnection()
+  repl({
+    connection,
+    executeSQL: (sql: string) => executeSQL(connection, sql),
+  })
 }
 
-main();
+async function init() {
+  process.chdir(__dirname + "/..")
+  await loadEnv("")
+  configureLogging()
+}
 
-// TODO CLI option to open up a snowflake REPL
-// snowflakeRepl()
+const program = new Command()
+program.option("--cli", "open up a snowflake repl", false)
+program.parse(process.argv)
+const options = program.opts()
 
-function cleanSnowflakeSQLDescribe(describeMap) {
-  // # column filters
-  // uppercase takes more tokens, lowercase everything
-  // "create or replace" => "create"
-  // VARCHAR(\d) => TEXT
-  // "cluster by (.*)" => remove airbyte stuff
-  // SCD => slowly changing dimension tables, filter these
-  // exclude airbyte columns
-  // # table filters
-  // exclude tables that do not have any data
-  // exclude columns without any values
-  // manually exclude some tables they aren't going to use
+if (options.cli) {
+  snowflakeRepl()
+} else {
+  main("")
 }
 
 // var snowflakeTableJSON = require('fs').readFileSync('./snowflake.json')
