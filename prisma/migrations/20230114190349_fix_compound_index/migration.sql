71f987356b7dda353931559c347ea57424bc8631
Michael Bianco <iloveitaly@gmail.com>
2023-01-19 14:41:25 -0700

    feat(vector): reset migrations

--- prisma/migrations/20230114190349_fix_compound_index/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "Question_userId_id_questionGroupId_key";
-
--- CreateIndex
-CREATE INDEX "Question_userId_id_questionGroupId_idx" ON "Question"("userId", "id", "questionGroupId");
