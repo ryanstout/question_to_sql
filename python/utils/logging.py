0fb6de7f5ed6cd1db5b783d9b89e046db35e66a8
Michael Bianco <iloveitaly@gmail.com>
2023-01-23 15:33:42 -0700

    feat(snowflake): logging

---
+++ python/utils/logging.py
@@ -0,0 +1,18 @@
+import logging
+import structlog
+from decouple import config
+
+def setLevel(level):
+    level = getattr(logging, level.upper())
+    structlog.configure(
+        # context_class enables thread-local logging to avoid passing a log instance around
+        # https://www.structlog.org/en/21.1.0/thread-local.html
+        # context_class=wrap_dict(dict),
+        # wrapper_class=structlog.make_filtering_bound_logger(level),
+        cache_logger_on_first_use=True,
+    )
+
+log_level = config("LOG_LEVEL", default="WARN")
+setLevel(log_level)
+
+log = structlog.get_logger()
