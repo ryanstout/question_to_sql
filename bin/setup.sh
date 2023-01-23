75555ca95a15d19acd723c2e8e7fc4dad661c98c
Michael Bianco <iloveitaly@gmail.com>
2023-01-23 12:08:09 -0700

    dev: update setup script & python version to play well with pytorch and others

--- bin/setup.sh
+++ bin/setup.sh
@@ -15,17 +15,26 @@ npm install
 echo "Install libomp openblas for Mac for Faiss kNN library"
 brew install libomp openblas
 
-
 cp .env.example .env
 echo "Update .env with fresh secrets"
 
+# get the remix application up and running
 npm run setup
 npm run build
 
 echo "`npm run dev` to start the server"
 
 # for better asdf compatibility
-poetry config virtualenvs.prefer-active-python true
+poetry config virtualenvs.prefer-activepython true
+poetry install
 
 # helpful packages for ipython
 pip install ipython ipdb pdbr ipython-autoimport rich docrepr colorama
+
+# you need python installed properly to generate the prisma bindings
+prisma generate --schema prisma/schema.prisma
+
+# for now, we are nuking the entire DB, will need to change this in the future
+psql --command="DROP DATABASE nlpquery" && \
+    rm -rf prisma/migrations && \
+    prisma migrate dev
