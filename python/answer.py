5909fc4216cf0194595af1fa267a1a1505c6cc54
Michael Bianco <iloveitaly@gmail.com>
2023-01-24 14:54:06 -0700

    fix: answer cli requirements

--- python/answer.py
+++ python/answer.py
@@ -1,13 +1,12 @@
+from python.setup import log
+
 import click
-
-from python.questions import question_with_data_source_to_sql
-
-# from python.setup import log
+from questions import question_with_data_source_to_sql
 
 
 @click.command()
-@click.option("--data-source-id", type=int)
-@click.option("--question", type=str)
+@click.option("--data-source-id", type=int, required=True)
+@click.option("--question", type=str, required=True)
 def cli(data_source_id, question):
     sql = question_with_data_source_to_sql(data_source_id, question)
     print(sql)
--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,6 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from .utils.logging import log
