1ce0bfcbbd19ed1f724fc20642794a4a194068e3
Michael Bianco <iloveitaly@gmail.com>
2023-01-25 17:16:30 -0700

    not working backoff for openai stuff

--- python/embeddings/openai_embeddings.py
+++ python/embeddings/openai_embeddings.py
@@ -2,18 +2,22 @@ from python.setup import log
 
 import os
 
+import backoff_utils
 import numpy as np
 import openai
-from backoff_utils import apply_backoff
+from backoff_utils import apply_backoff, strategies
 from decouple import config
 
 openai.api_key = config("OPENAI_KEY")
 
 
 class OpenAIEmbeddings:
-    # @apply_backoff(max_tries=5)
+    # https://backoff-utils.readthedocs.io/en/latest/strategies.html
+    # if we've hit an openAI limit, we need to wait awhile
+    @apply_backoff(max_tries=5, strategy=strategies.Exponential(minimum=60))
     def encode(content: str):
-        # log.debug("Encoding with OpenAI", content=content)
+        log.debug("encoding with OpenAI", content=content)
+
         result = openai.Embedding.create(model="text-embedding-ada-002", input=content)
 
         emb = np.array(result.data[0].embedding, dtype=np.float32)
