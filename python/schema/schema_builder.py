4da753f699aa64e1dabc4d7442dca937f16e271c
Michael Bianco <iloveitaly@gmail.com>
2023-01-25 20:30:04 -0700

    adding ids to all tables

--- python/schema/schema_builder.py
+++ python/schema/schema_builder.py
@@ -1,6 +1,9 @@
 # Create the schema for a DataSource by querying the embedding indexes and
 # deciding which tables, columns, and value hints to include in the prompt.
 
+# always include ID, include ID first, NOT NULL annotations, all `_id` columns on all tables that we include
+# order the columns naturally
+
 from python.setup import log
 
 import re
@@ -19,7 +22,7 @@ from prisma.models import (
 import python.utils as utils
 from prisma import Prisma
 from python.schema.full_schema import FullSchema
-from python.schema.ranker import SCHEMA_RANKING_TYPE, ElementRank
+from python.schema.ranker import SCHEMA_RANKING_TYPE, ElementRank, Ranker
 from python.sql.post_transform import PostTransform
 
 # TODO looks like we cannot set default values on typeddict: https://github.com/python/mypy/issues/6131
@@ -113,6 +116,48 @@ class SchemaBuilder:
     def generate_sql_describe(self, table_ranks: TABLE_SCHEMA) -> str:
         return "\n".join([self.generate_sql_table_describe(table_rank) for table_rank in table_ranks])
 
+    def create_table_rank(self, tableId: int) -> TableRank:
+        table = self.db.datasourcetabledescription.find_first(where={"id": tableId})
+
+        # we want to include the primary key and all foreign keys by default as columns
+        id_columns = self.db.datasourcetablecolumn.find_many(
+            where={
+                "AND": [
+                    {"dataSourceTableDescriptionId": tableId},
+                    {
+                        "OR": [
+                            {
+                                "name": {
+                                    # TODO should allow for endsWith instead!
+                                    "endswith": "_ID",
+                                }
+                            },
+                            {"name": "ID"},
+                        ]
+                    },
+                ],
+            }
+        )
+
+        default_id_columns = [
+            self.create_column_rank(ElementRank(table_id=tableId, column_id=column.id, value_hint=None, score=0.0)) for column in id_columns
+        ]
+
+        return {
+            "fully_qualified_name": table.fullyQualifiedName,
+            "table_id": table.id,
+            "columns": default_id_columns,
+        }
+
+    def create_column_rank(self, column_rank: ElementRank) -> ColumnRank:
+        column = self.db.datasourcetablecolumn.find_first(where={"id": column_rank["column_id"]})
+
+        return {
+            "name": column.name,
+            "column_id": column.id,
+            "hints": [column_rank["value_hint"]],
+        }
+
     def generate_sql_from_element_rank(self, ranked_schema: SCHEMA_RANKING_TYPE) -> str:
         # we need to transform the ranking schema into a list of table
         table_ranks: t.List[TableRank] = []
@@ -133,12 +178,7 @@ class SchemaBuilder:
             table_rank = next((table_rank for table_rank in table_ranks if table_rank["table_id"] == element_rank["table_id"]), None)
 
             if not table_rank:
-                table = self.db.datasourcetabledescription.find_first(where={"id": element_rank["table_id"]})
-                table_rank: TableRank = {
-                    "fully_qualified_name": table.fullyQualifiedName,
-                    "table_id": table.id,
-                    "columns": [],
-                }
+                table_rank = self.create_table_rank(element_rank["table_id"])
                 table_ranks.append(table_rank)
 
             if element_rank["column_id"]:
@@ -151,16 +191,10 @@ class SchemaBuilder:
                     # add the hint to the column_rank
                     column_rank["hints"].append(element_rank["value_hint"])
                 else:
-                    # create a new column_rank
-                    column = self.db.datasourcetablecolumn.find_first(where={"id": column_id})
-                    column_rank: ColumnRank = {
-                        "name": column.name,
-                        "column_id": column.id,
-                        "hints": [element_rank["value_hint"]],
-                    }
+                    column_rank = self.create_column_rank(element_rank)
                     table_rank["columns"].append(column_rank)
 
-        token_count = self.estimate_token_count(new_table_sql)
+        token_count = estimate_token_count(new_table_sql)
         log.debug("schema token count", token_count=token_count)
         return sql
 
@@ -171,57 +205,10 @@ if __name__ == "__main__":
     connections = Connections()
     db = connections.open()
 
-    datasource = db.datasource.find_first()
+    datasource = db.datasource.find_first(where={"id": 2})
+
+    ranks = Ranker(connections.db, 2).rank("What product sells best in Montana?")
 
     # generate via: `poetry run python -m python.schema.ranker "What product sells best in Montana?"`
-    ranking_data: t.List[ElementRank] = [
-        {"table_id": 8, "column_id": 166, "value_hint": "best-selling", "score": 0.8065115213394165},
-        {"table_id": 8, "column_id": 166, "value_hint": "best-selling", "score": 0.8065115213394165},
-        {"table_id": 8, "column_id": 166, "value_hint": "best-selling", "score": 0.8065115213394165},
-        {"table_id": 9, "column_id": 174, "value_hint": None, "score": 0.7583510875701904},
-        {"table_id": 9, "column_id": 174, "value_hint": None, "score": 0.7583510875701904},
-        {"table_id": 4, "column_id": 119, "value_hint": None, "score": 0.7519586682319641},
-        {"table_id": 4, "column_id": 119, "value_hint": None, "score": 0.7519586682319641},
-        {"table_id": 6, "column_id": 155, "value_hint": "Avalara", "score": 0.7475363612174988},
-        {"table_id": 6, "column_id": 155, "value_hint": "Avalara", "score": 0.7475363612174988},
-        {"table_id": 6, "column_id": 155, "value_hint": "Avalara", "score": 0.7475363612174988},
-        {"table_id": 4, "column_id": 113, "value_hint": None, "score": 0.7455527782440186},
-        {"table_id": 6, "column_id": 152, "value_hint": None, "score": 0.7443681359291077},
-        {"table_id": 4, "column_id": 120, "value_hint": None, "score": 0.7443681359291077},
-        {"table_id": 1, "column_id": 6, "value_hint": None, "score": 0.7430565357208252},
-        {"table_id": 8, "column_id": 167, "value_hint": None, "score": 0.7430565357208252},
-        {"table_id": 6, "column_id": 154, "value_hint": None, "score": 0.7430565357208252},
-        {"table_id": 4, "column_id": 123, "value_hint": None, "score": 0.7430565357208252},
-        {"table_id": 1, "column_id": 6, "value_hint": None, "score": 0.7430565357208252},
-        {"table_id": 10, "column_id": 175, "value_hint": None, "score": 0.7422063946723938},
-        {"table_id": 10, "column_id": 175, "value_hint": None, "score": 0.7422063946723938},
-        {"table_id": 6, "column_id": 159, "value_hint": None, "score": 0.7405303716659546},
-        {"table_id": 3, "column_id": 97, "value_hint": "percentage", "score": 0.7401782274246216},
-        {"table_id": 3, "column_id": 97, "value_hint": "percentage", "score": 0.7401782274246216},
-        {"table_id": 3, "column_id": 97, "value_hint": "percentage", "score": 0.7401782274246216},
-        {"table_id": 1, "column_id": 2, "value_hint": None, "score": 0.7391866445541382},
-        {"table_id": 4, "column_id": 110, "value_hint": None, "score": 0.7382106781005859},
-        {"table_id": 6, "column_id": 157, "value_hint": None, "score": 0.7376145124435425},
-        {"table_id": 10, "column_id": 176, "value_hint": None, "score": 0.734853982925415},
-        {"table_id": 8, "column_id": 166, "value_hint": "price-asc", "score": 0.7295647859573364},
-        {"table_id": 8, "column_id": 166, "value_hint": "manual", "score": 0.7221354246139526},
-        {"table_id": 8, "column_id": 166, "value_hint": "created-desc", "score": 0.7215535640716553},
-        {"table_id": 9, "column_id": None, "value_hint": None, "score": 0.7204369306564331},
-        {"table_id": 8, "column_id": 172, "value_hint": "web", "score": 0.7185894250869751},
-        {"table_id": 8, "column_id": 172, "value_hint": "web", "score": 0.7185894250869751},
-        {"table_id": 8, "column_id": 170, "value_hint": "shogun", "score": 0.7158838510513306},
-        {"table_id": 8, "column_id": 170, "value_hint": "shogun", "score": 0.7158838510513306},
-        {"table_id": 5, "column_id": None, "value_hint": None, "score": 0.7091584205627441},
-        {"table_id": 2, "column_id": None, "value_hint": None, "score": 0.7083749175071716},
-        {"table_id": 2, "column_id": 26, "value_hint": None, "score": 0.7042477130889893},
-        {
-            "table_id": 2,
-            "column_id": 26,
-            "value_hint": "Engraving: Always Faithful, Always Strong\nFont: Script\nRed Filled Engraving",
-            "score": 0.7008172273635864,
-        },
-        {"table_id": 3, "column_id": 97, "value_hint": "fixed_amount", "score": 0.6898645758628845},
-        {"table_id": 7, "column_id": None, "value_hint": None, "score": 0.6895037889480591},
-    ]
 
-    print(SchemaBuilder(db).build(ranking_data))
+    print(SchemaBuilder(db).build(ranks))
