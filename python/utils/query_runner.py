5cbdbf51b331ac94326d3b861c5b9813dbf91f68
Ryan Stout <ryanstout@gmail.com>
2023-01-28 10:05:48 -0700

    add default limit to snowflake queries

--- python/utils/query_runner.py
+++ python/utils/query_runner.py
@@ -1,4 +1,6 @@
 import json
+import re
+import time
 
 import snowflake.connector as connector
 
@@ -14,16 +16,23 @@ def setup_query_env(snowflake_cursor):
 def run_query(snowflake_cursor, sql):
     try:
         log.debug("running query", sql=sql)
+
+        if not re.search("\sLIMIT\s", sql):
+            sql += " LIMIT 100"
+
+        t1 = time.time()
         results = snowflake_cursor.execute(sql)
-        log.debug("result: ", results=results)
 
         # Copy data out now so we can reuse the cursor
         data = []
         for result in results:
             data.append(result)
 
+        t2 = time.time()
+        log.debug("query ran in: ", time=t2 - t1)
+
         # Return the result of the query, not the uses
         return data
     except connector.errors.ProgrammingError as e:
         log.error("snowflake connector programming error: ", error=e)
-        return str(e)
+        return {"error": str(e)}
