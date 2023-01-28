73aee2043664f243f009ad847626ab6414930efe
Michael Bianco <iloveitaly@gmail.com>
2023-01-28 12:28:29 -0700

    adding sentry config

--- python/utils/logging.py
+++ python/utils/logging.py
@@ -36,4 +36,15 @@ def configureLogger():
 
 configureLogger()
 
+if t.cast(str, config("PYTHON_ENV", default="development", type=str)).lower() == "production":
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn=t.cast(str, config("SENTRY_DSN", cast=str, required=True)),
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+    )
+
 log = structlog.get_logger()
