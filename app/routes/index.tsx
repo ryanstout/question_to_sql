23c2dc20500a573ca080411ddc52b0706e6f196a
Michael Bianco <iloveitaly@gmail.com>
2023-01-30 10:43:54 -0700

    redirect right to the primary query page
    we can revert this later, but it's weird dropping on the empty page and then
    redirecting the user

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,46 +1,5 @@
-import { AppShell } from "@mantine/core"
-import type { ActionArgs } from "@remix-run/node"
-import { json } from "@remix-run/node"
-import { useActionData } from "@remix-run/react"
-import { z } from "zod"
-import { zx } from "zodix"
-import AppFooter from "~/components/dashboard/footer"
-import HeaderMenu from "~/components/dashboard/headerMenu"
-import LandingSearch from "~/components/dashboard/landingSearch"
-import { processQuestion } from "~/lib/question.server"
-import { requireUserId } from "~/session.server"
-
-
-
-
-// this is executed on the server side
-export const action = async ({ request }: ActionArgs) => {
-  const { q } = await zx.parseForm(request, {
-    q: z.string(),
-  })
-
-  const userId = await requireUserId(request)
-  const dataSourceId = 1
-  const result = await processQuestion(userId, dataSourceId, q)
-
-  return json({ q, sql })
-}
+import { redirect } from "@remix-run/server-runtime"
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const userId = await requireUserId(request)
-  return userId;
-}
-
-export default function Index() {
-  const loader = useActionData()
-
-  return (
-    <AppShell footer={<AppFooter />}>
-      <HeaderMenu />
-      <LandingSearch question={loader ? loader.q : ""} />
-    </AppShell>
-  )
-}
-function useRequiredUser() {
-  throw new Error("Function not implemented.")
+  return redirect("/admin/1/chart/raw")
 }
