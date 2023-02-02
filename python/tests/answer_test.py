e23884c09a911ca0e69565978e2ef50dd163f9ac
Michael Bianco <iloveitaly@gmail.com>
2023-02-02 06:34:24 -0700

    refactor(python): using pytest-flask for setup

--- python/tests/answer_test.py
+++ python/tests/answer_test.py
@@ -3,27 +3,6 @@ import pytest
 from python.server import application
 
 
-@pytest.fixture()
-def app():
-    application.config.update(
-        {
-            "TESTING": True,
-        }
-    )
-
-    yield application
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
-
-
 def test_basic_answer(client):
     response = client.post(
         "/question",
--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,3 +1,10 @@
 from python.setup import log
 
 import pytest
+
+from python.server import application
+
+
+@pytest.fixture()
+def app():
+    return application
