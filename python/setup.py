8a53b16fdc9613eeef270a287b1fcdf8a3e24d87
Michael Bianco <iloveitaly@gmail.com>
2023-02-08 06:56:25 -0700

    refactor: move faulthandler to commomn setup
    @ryanstout is there a reason why we wouldn't enable this in all non-prod environments?

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,17 @@
+# isort:skip_file
+
 # make pythonpath the upper directory relative to the location of this file
 import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from .utils.logging import log
+
+from utils.system import is_production
+from utils.logging import log
+
+import faulthandler
+
+# Setup the fault handler so we can send SIGABRT and get a stack trace
+if not is_production():
+    faulthandler.enable()
