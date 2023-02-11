73997b48fd0f6ec4ce3d5b254a5f81387dd2f81e
Michael Bianco <iloveitaly@gmail.com>
2023-02-11 14:30:05 -0700

    fix: new migrations
    I'm sorry...I don't want to deal w/prisma migrations yet

--- prisma/migrations/20230211212915_initial/migration.sql
+++ prisma/migrations/20230211212915_initial/migration.sql
@@ -2,6 +2,12 @@
 CREATE TYPE "FeedbackState" AS ENUM ('INVALID', 'UNANSWERED', 'UNKNOWN', 'CORRECT', 'INCORRECT');
 
 -- CreateEnum
+CREATE TYPE "EvaluationOperators" AS ENUM ('EQUALS', 'CONTAINS');
+
+-- CreateEnum
+CREATE TYPE "EvaluationStatus" AS ENUM ('UNREAD', 'INCOMPLETE', 'CORRECT');
+
+-- CreateEnum
 CREATE TYPE "DataSourceType" AS ENUM ('SNOWFLAKE', 'POSTGRES');
 
 -- CreateTable
@@ -23,31 +29,60 @@ CREATE TABLE "Password" (
 );
 
 -- CreateTable
-CREATE TABLE "QuestionGroup" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "correctSql" TEXT,
-
-    CONSTRAINT "QuestionGroup_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Question" (
     "id" SERIAL NOT NULL,
     "question" TEXT NOT NULL,
     "dataSourceId" INTEGER NOT NULL,
     "userId" INTEGER NOT NULL,
-    "questionGroupId" INTEGER NOT NULL,
-    "generatedSchema" TEXT,
-    "generatedPrompt" TEXT,
-    "codexSql" TEXT,
+    "sql" TEXT,
     "userSql" TEXT,
-    "resultData" JSONB,
     "feedbackState" "FeedbackState" NOT NULL DEFAULT 'UNANSWERED',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "EvaluationQuestionGroup" (
+    "id" SERIAL NOT NULL,
+    "status" "EvaluationStatus" NOT NULL DEFAULT 'UNREAD',
+    "dataSourceId" INTEGER NOT NULL,
+    "correctSql" TEXT,
+    "result" JSONB,
+    "resultOperator" "EvaluationOperators" NOT NULL DEFAULT 'EQUALS',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "EvaluationQuestionGroup_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "EvaluationQuestion" (
+    "id" SERIAL NOT NULL,
+    "evaluationQuestionGroupId" INTEGER NOT NULL,
+    "fromQuestionId" INTEGER,
+    "generatedSchema" TEXT,
+    "generatedPrompt" TEXT,
+    "question" TEXT NOT NULL,
+    "codexSql" TEXT,
+    "notes" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "EvaluationQuestion_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "EvaluationResult" (
+    "id" SERIAL NOT NULL,
+    "successfulQuestions" JSONB NOT NULL,
+    "failedQuestions" JSONB NOT NULL,
+    "evaluatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "EvaluationResult_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -119,7 +154,7 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Question_id_userId_questionGroupId_key" ON "Question"("id", "userId", "questionGroupId");
+CREATE UNIQUE INDEX "Question_id_userId_key" ON "Question"("id", "userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "DataSourceTableDescription_dataSourceId_fullyQualifiedName_key" ON "DataSourceTableDescription"("dataSourceId", "fullyQualifiedName");
@@ -131,16 +166,19 @@ ALTER TABLE "User" ADD CONSTRAINT "User_businessId_fkey" FOREIGN KEY ("businessI
 ALTER TABLE "Password" ADD CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "QuestionGroup" ADD CONSTRAINT "QuestionGroup_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Question" ADD CONSTRAINT "Question_dataSourceId_fkey" FOREIGN KEY ("dataSourceId") REFERENCES "DataSource"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Question" ADD CONSTRAINT "Question_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Question" ADD CONSTRAINT "Question_questionGroupId_fkey" FOREIGN KEY ("questionGroupId") REFERENCES "QuestionGroup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "EvaluationQuestionGroup" ADD CONSTRAINT "EvaluationQuestionGroup_dataSourceId_fkey" FOREIGN KEY ("dataSourceId") REFERENCES "DataSource"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EvaluationQuestion" ADD CONSTRAINT "EvaluationQuestion_evaluationQuestionGroupId_fkey" FOREIGN KEY ("evaluationQuestionGroupId") REFERENCES "EvaluationQuestionGroup"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EvaluationQuestion" ADD CONSTRAINT "EvaluationQuestion_fromQuestionId_fkey" FOREIGN KEY ("fromQuestionId") REFERENCES "Question"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "DataSource" ADD CONSTRAINT "DataSource_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "Business"("id") ON DELETE SET NULL ON UPDATE CASCADE;
