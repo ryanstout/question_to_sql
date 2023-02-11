e3988ee115e7089c26c28e70baf4ecbb900eab7e
Michael Bianco <iloveitaly@gmail.com>
2023-02-11 14:31:12 -0700

    fix: crypress ts fix

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -20,7 +20,7 @@ async function createAndLogin(email: string) {
     throw new Error("All test emails must end in @example.com")
   }
 
-  const user = await createUser(email, "myreallystrongpassword")
+  const user = await createUser(email, "myreallystrongpassword", "mike")
 
   const response = await createUserSession({
     request: new Request("test://test"),
