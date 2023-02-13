62f098d5991cfeb584ca9892feeea8c4e478a6d2
Michael Bianco <iloveitaly@gmail.com>
2023-02-13 12:34:37 -0700

    refactor: use isProduction helper for sentry setup

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -6,8 +6,10 @@ import { RemixBrowser, useLocation, useMatches } from "@remix-run/react"
 import { ClientProvider } from "@mantine/remix"
 
 import * as Sentry from "@sentry/remix"
+import { isProduction } from "./lib/environment.server"
 
 // TODO may break emotion on the client side https://github.com/emotion-js/emotion/issues/2800
+// TODO this is not correct, we should be using hydrateRoot, but it doesn't look like mantine supports this yet app/entry.client.tsx
 hydrate(
   <ClientProvider>
     <RemixBrowser />
@@ -15,8 +17,7 @@ hydrate(
   document
 )
 
-// TODO should use helper for this
-if (process.env.NODE_ENV === "production") {
+if (isProduction()) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     tracesSampleRate: 1,
