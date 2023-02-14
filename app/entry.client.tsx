01f6851762dbb03ed7e83ebdc547cb3cf617b034
Michael Bianco <iloveitaly@gmail.com>
2023-02-13 19:27:08 -0700

    Revert "Revert "fix: Fix hydrateRoot issue""
    This reverts commit 5438e2e2f292df7afb1e8dec25f8d2461659780f.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,20 +1,23 @@
 import { useEffect } from "react"
-import { hydrate } from "react-dom"
 
 import { RemixBrowser, useLocation, useMatches } from "@remix-run/react"
 
 import { ClientProvider } from "@mantine/remix"
 
 import * as Sentry from "@sentry/remix"
-import { isProduction } from "./lib/environment.server"
+import { hydrateRoot } from "react-dom/client"
+
+export function isProduction() {
+  return process.env.NODE_ENV === "production"
+}
 
 // TODO may break emotion on the client side https://github.com/emotion-js/emotion/issues/2800
 // TODO this is not correct, we should be using hydrateRoot, but it doesn't look like mantine supports this yet app/entry.client.tsx
-hydrate(
+hydrateRoot(
+  document,
   <ClientProvider>
     <RemixBrowser />
-  </ClientProvider>,
-  document
+  </ClientProvider>
 )
 
 if (isProduction()) {
