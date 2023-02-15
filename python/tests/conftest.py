838be7bfba0d4b1844e4e6a07081c975f35b9b14
Michael Bianco <iloveitaly@gmail.com>
2023-02-15 15:00:31 -0700

    test(python): force PYTHON_ENV in conftest, wipe redis on each run

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,10 +1,28 @@
+# isort:skip_file
+# NOTE order of operations really matters here!
+
+import sys, os
+
+from python.utils.redis import application_redis_connection
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+
 from python.setup import log
 
 import pytest
 
 from python.server import application
 
+os.environ["PYTHON_ENV"] = "testing"
+
+redis = application_redis_connection()
+
 
 @pytest.fixture()
 def app():
     return application
+
+
+@pytest.fixture(scope="function", autouse=True)
+def wipe_redis_and_database():
+    redis.flushall()
