c524228261dc54f3d0d5d80316ed21b546f9cf8c
Michael Bianco <iloveitaly@gmail.com>
2023-02-15 11:17:36 -0700

    build: add flask integration to sentry

--- python/utils/sentry.py
+++ python/utils/sentry.py
@@ -1,18 +1,23 @@
 import typing as t
 
+import sentry_sdk
 from decouple import config
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from python.utils.system import is_production
 
 
 def configure_sentry():
-    if is_production():
-        import sentry_sdk
+    if not is_production():
+        return
 
-        sentry_sdk.init(
-            dsn=t.cast(str, config("SENTRY_DSN", cast=str)),
-            # Set traces_sample_rate to 1.0 to capture 100%
-            # of transactions for performance monitoring.
-            # We recommend adjusting this value in production.
-            traces_sample_rate=1.0,
-        )
+    sentry_sdk.init(
+        dsn=t.cast(str, config("SENTRY_DSN", cast=str)),
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        integrations=[
+            FlaskIntegration(),
+        ],
+    )
