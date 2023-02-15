e307d7daa4e90b53b1ef3304fd0220e99bfc6faa
Michael Bianco <iloveitaly@gmail.com>
2023-02-15 16:04:19 -0700

    fix: removing duplicate isProduction

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,12 +5,10 @@ import { RemixBrowser, useLocation, useMatches } from "@remix-run/react"
 
 import { ClientProvider } from "@mantine/remix"
 
+import { isProduction } from "~/lib/environment.server"
+
 import * as Sentry from "@sentry/remix"
 import { hydrateRoot } from "react-dom/client"
-
-export function isProduction() {
-  return process.env.NODE_ENV === "production"
-}
 
 // TODO may break emotion on the client side https://github.com/emotion-js/emotion/issues/2800
 // TODO this is not correct, we should be using hydrateRoot, but it doesn't look like mantine supports this yet app/entry.client.tsx
