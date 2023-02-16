4752b4cc981cc6c574a0b5df90c72083bd604c3d
Michael Bianco <iloveitaly@gmail.com>
2023-02-15 17:29:10 -0700

    feat: prompt construction improvements

--- python/questions/__init__.py
+++ python/questions/__init__.py
@@ -1,5 +1,6 @@
-import utils
+import datetime
 
+import python.utils.db
 from python.schema.ranker import Ranker
 from python.schema.schema_builder import SchemaBuilder
 from python.sql.post_transform import PostTransform
@@ -7,7 +8,7 @@ from python.utils.batteries import log_execution_time
 from python.utils.logging import log
 from python.utils.openai_rate_throttled import openai_throttled
 
-db = utils.db.application_database_connection()
+db = python.utils.db.application_database_connection()
 
 
 def question_with_data_source_to_sql(data_source_id: int, question: str, engine: str = "code-davinci-002") -> str:
@@ -30,6 +31,8 @@ def question_with_data_source_to_sql(data_source_id: int, question: str, engine:
     with log_execution_time("schema to sql"):
         sql = question_with_schema_to_sql(table_schema_limited_by_token_size, question)
 
+    log.debug("sql pre transform", sql=sql)
+
     # AST based transform of the SQL to fix up common issues
     with log_execution_time("ast transform"):
         sql = PostTransform(data_source_id).run(sql)
@@ -40,32 +43,27 @@ def question_with_data_source_to_sql(data_source_id: int, question: str, engine:
 
 
 def question_with_schema_to_sql(schema: str, question: str, engine: str = "code-davinci-002") -> str:
+    def generate_date_suffix(n):
+        if 10 <= n % 100 < 20:
+            return "th"
+
+        return {1: "st", 2: "nd", 3: "rd"}.get(n % 10, "th")
+
+    day_int = datetime.datetime.now().day
+    suffix = generate_date_suffix(day_int)
+
+    # current date in the format of 'January 28th, 2023'
+    # the date format here is picked in a very specific fashion
+    current_date = datetime.datetime.now().strftime(f"%B %-d{suffix}, %Y")
+
     prompt_parts = [
         f"-- {PostTransform.in_dialect.capitalize()} SQL schema",
         schema,
         "",
-        # TODO is this few shot learning? Can we extract this out?
-        "-- How many orders are there per month?",
-        'SELECT\n  COUNT(*) AS orders_per_month,\n  EXTRACT(MONTH FROM created_at) AS month\nFROM "ORDER"\nGROUP BY\n  month\nORDER BY\n  month NULLS LAST',
-        "",
-        "-- Which product sells the best?",
-        """SELECT
-  COUNT(*) AS orders_per_product,
-  PRODUCT.title
-FROM "ORDER"
-JOIN ORDER_LINE
-  ON ORDER_LINE.order_id = "ORDER".id
-JOIN PRODUCT_VARIANT
-  ON PRODUCT_VARIANT.id = ORDER_LINE.variant_id
-JOIN PRODUCT
-  ON PRODUCT.id = PRODUCT_VARIANT.product_id
-GROUP BY
-  PRODUCT.title
-ORDER BY
-orders_per_product DESC NULLS FIRST""",
-        "",
-        # "-- Assuming the current date is January 28th, 2023",
-        # "-- using NOW() instead of dates\n\n",
+        # "-- Treat the tables listed below in order of priority, highest priority being listed first",
+        f"-- Assuming the current date is {current_date}",
+        # an extra line break hints to the model that what comes next is different and should be treated as something different
+        "-- using NOW() instead of dates\n\n",
         f"-- {question}",
         "SELECT ",
     ]
