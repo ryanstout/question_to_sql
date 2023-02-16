423a26dbdeb8b0b8a4a7c80959ff1117582956a4
Kailash <kaidurai@gmail.com>
2023-02-15 18:20:57 -0700

    feat: Mantine DataTable in history display

--- app/routes/history/index.tsx
+++ app/routes/history/index.tsx
@@ -1,9 +1,11 @@
+import { DataTable } from "mantine-datatable"
 import invariant from "tiny-invariant"
 
-import { LoaderArgs, json } from "@remix-run/node"
+import type { LoaderArgs } from "@remix-run/node"
+import { json } from "@remix-run/node"
 import { Link, useLoaderData } from "@remix-run/react"
 
-import { Button, Grid, Table } from "@mantine/core"
+import { Box, Button, Grid } from "@mantine/core"
 
 import type { HistoryResult } from "~/lib/history.server"
 import { getUserQuestionHistory } from "~/lib/history.server"
@@ -30,44 +32,54 @@ const HistoryDisplayComponent = ({
 }: {
   historyResult: HistoryResult
 }) => {
-  const rows = historyResult.questionHistory.map((question) => (
-    <tr key={question.id}>
-      <td>{question.question}</td>
-      <td>
-        {new Date(question.createdAt).toLocaleDateString("en-US")} at{" "}
-        {new Date(question.createdAt).toLocaleTimeString([], {
-          timeStyle: "short",
-        })}
-      </td>
-      <td>
-        <Link
-          target="_blank"
-          to={`/question/${question.id}`}
-          style={{ all: "unset" }}
-        >
-          <Button variant="subtle" rightIcon={<IconExternalLink size={16} />}>
-            View
-          </Button>
-        </Link>
-      </td>
-    </tr>
-  ))
-
   return (
     <Grid>
       <Grid.Col span={10} offset={1}>
-        <Table verticalSpacing="sm" mt={75}>
-          <thead>
-            <tr>
-              <th style={{ wordWrap: "break-word", textAlign: "center" }}>
-                Question
-              </th>
-              <th style={{ textAlign: "center" }}>Created</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </Table>
+        <Box sx={{ height: 450 }} mt={80}>
+          <DataTable
+            verticalSpacing="md"
+            withBorder
+            columns={[
+              {
+                accessor: "question",
+                title: "Question History",
+                textAlignment: "center",
+              },
+              {
+                accessor: "Asked",
+                textAlignment: "center",
+                render: (record) => {
+                  return `${new Date(record.createdAt).toLocaleDateString(
+                    "en-US"
+                  )} at ${new Date(record.createdAt).toLocaleTimeString([], {
+                    timeStyle: "short",
+                  })}`
+                },
+              },
+              {
+                accessor: "",
+                textAlignment: "center",
+                render: (record) => {
+                  return (
+                    <Link
+                      target="_blank"
+                      to={`/question/${record.id}`}
+                      style={{ all: "unset" }}
+                    >
+                      <Button
+                        variant="subtle"
+                        rightIcon={<IconExternalLink size={16} />}
+                      >
+                        View
+                      </Button>
+                    </Link>
+                  )
+                },
+              },
+            ]}
+            records={historyResult.questionHistory}
+          />
+        </Box>
       </Grid.Col>
     </Grid>
   )
@@ -78,7 +90,7 @@ export default function HistoryIndexView() {
     typeof loader
   >() as unknown as HistoryResult
 
-  //TODO: Use Mantine DataTable component for searchable table
+  //TODO: Add searching for DataTable
   //TODO: Add pagination to table with backend Prisma
 
   return (
