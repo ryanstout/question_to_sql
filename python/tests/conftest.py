769ca9249f06a91f2d82f2e235c390baa75ea38e
Michael Bianco <iloveitaly@gmail.com>
2023-02-21 06:05:01 -0700

    test: make sure PYTHON_ENV is set first when setting up tests
    this was causing tests to fail to run on prod

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,28 +1,26 @@
-# isort:skip_file
+# isort: skip_file
+# fmt: off
+
 # NOTE order of operations really matters here!
+import os
+os.environ["PYTHON_ENV"] = "test"
 
-import sys, os
-
-from python.utils.redis import application_redis_connection
-
+import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 
 from python.setup import log
 
 import pytest
 
-from python.server import application
-
-os.environ["PYTHON_ENV"] = "test"
-
+# wipe redis on each run: make sure you are using a separate redis from dev!
+from python.utils.redis import application_redis_connection
 redis = application_redis_connection()
-
-
-@pytest.fixture()
-def app():
-    return application
-
 
 @pytest.fixture(scope="function", autouse=True)
 def wipe_redis_and_database():
     redis.flushall()
+
+from python.server import application
+@pytest.fixture()
+def app():
+    return application
