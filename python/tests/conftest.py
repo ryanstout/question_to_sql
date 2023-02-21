a60b72c1f062157c336db796b7965867aa109f38
Michael Bianco <iloveitaly@gmail.com>
2023-02-21 08:08:02 -0700

    test: record embeddings test with vcr

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -12,6 +12,21 @@ from python.setup import log
 
 import pytest
 
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [
+            "Authorization"
+            # headers with random IDs which cause requests not to match, exclude these
+            # "Date",
+        ],
+        # "filter_query_parameters": ["signature", "timestamp"],
+        "decode_compressed_response": True,
+        "ignore_localhost": True,
+        # if libraries use `ping` or something similar, this reduces duplicate recordings
+        # "allow_playback_repeats": True,
+    }
 # wipe redis on each run: make sure you are using a separate redis from dev!
 from python.utils.redis import application_redis_connection
 redis = application_redis_connection()
--- python/tests/embeddings/test_embedding.py
+++ python/tests/embeddings/test_embedding.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pytest
 
 from python.embeddings.embedding import generate_embedding
 from python.embeddings.openai_embedder import OpenAIEmbedder
 
 
+@pytest.mark.vcr
 def test_embedding():
     e = generate_embedding("hello world", embedder=OpenAIEmbedder)
 
