fc352de168e49ad8ebc4963f93abfede7fa86c1b
Michael Bianco <iloveitaly@gmail.com>
2023-02-22 15:26:27 -0700

    feat(training): adding links to header if admin

--- app/components/dashboard/headerMenu.tsx
+++ app/components/dashboard/headerMenu.tsx
@@ -1,39 +1,56 @@
+import { $path } from "remix-routes"
+
 import { Form } from "@remix-run/react"
 
-import { Box, Button, Grid, Header, Image } from "@mantine/core"
+import { Button, Group, Header, Image } from "@mantine/core"
+
+import type { User } from "@prisma/client"
 
 import Logo from "~/assets/images/LogoHorizontal.svg"
 import { useOptionalUser } from "~/utils"
 
 import { IconLogout } from "@tabler/icons-react"
 
+function isAdmin(user: User) {
+  return user.email.endsWith("@knolbe.com")
+}
+
 export default function HeaderMenu() {
   const user = useOptionalUser()
 
-  let loginMenu
-  if (user) {
-    loginMenu = (
-      <Box mt={15} ta="right">
-        <Form action="/logout" method="post">
-          <Button variant="outline" type="submit">
-            Logout&nbsp;
-            <IconLogout />
-          </Button>
-        </Form>
-      </Box>
-    )
-  }
+  // TODO second empty param in $path is required, wait before rolling this out https://github.com/yesmeck/remix-routes/issues/43 */
+  const loginMenu = !user ? null : (
+    <Form action={$path("/logout", {})} method="post">
+      <Button variant="outline" type="submit">
+        Logout&nbsp;
+        <IconLogout />
+      </Button>
+    </Form>
+  )
+
+  const adminMenu =
+    user && isAdmin(user) ? (
+      <>
+        <Button component="a" href={$path("/internal", {})}>
+          Group Questions
+        </Button>
+        <Button component="a" href={$path("/internal/group", {})}>
+          Evaluation Groups
+        </Button>
+      </>
+    ) : null
 
   return (
     <Header height={80} px="md">
-      <Grid>
-        <a href="/">
-          <Image height={70} width={180} mt={11} src={Logo} />
+      <>
+        <a style={{ float: "left" }} href="/">
+          <Image height={70} width={180} mt={5} src={Logo} />
         </a>
-        <Grid.Col span={1} offset={9}>
+        <Group mt={20} position="right">
+          {adminMenu}
           {loginMenu}
-        </Grid.Col>
-      </Grid>
+        </Group>
+      </>
     </Header>
   )
 }
