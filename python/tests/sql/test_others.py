48d8fd6bc4f901e6dc6cc79da159a42903e30812
Michael Bianco <iloveitaly@gmail.com>
2023-02-23 07:41:36 -0700

    test: add failing sql inspector test case
    @ryanstout
    
    * Ran into this while testing training ui stuff
    * Test case is failing, assertion is wrong
    
    I can dig into this if it's not a very quick fix for you!

--- python/tests/sql/test_others.py
+++ python/tests/sql/test_others.py
@@ -17,6 +17,55 @@ def test_static_alias():
     assert inspector.touches() == {}
 
 
+def test_string_concat():
+    inspector = SqlInspector(
+        """
+        SELECT 'a' || 'b' AS col1;
+        """,
+        BASIC_SCHEMA_STRUCTURE,
+    )
+
+    assert inspector.touches() == {}
+
+
+# TODO this is failing!
+def test_complex_string_concat():
+    inspector = SqlInspector(
+        """
+        SELECT
+            COUNT(*) AS orders_per_customer,
+            customer.first_name || ' ' || customer.last_name AS full_name
+        FROM "order"
+        JOIN customer
+            ON customer.id = "order".customer_id
+        GROUP BY
+            customer.first_name,
+            customer.last_name
+        ORDER BY
+            orders_per_customer DESC
+        LIMIT 5;
+        """,
+        {
+            "order": {
+                "name": "order",
+                "columns": {
+                    "customer_id": {"name": "customer_id"},
+                },
+            },
+            "customer": {
+                "name": "customer",
+                "columns": {
+                    "id": {"name": "id"},
+                    "first_name": {"name": "first_name"},
+                    "last_name": {"name": "last_name"},
+                },
+            },
+        },
+    )
+
+    assert inspector.touches() == {}
+
+
 def test_sum_propagation():
     inspector = SqlInspector(
         """
