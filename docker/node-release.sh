177916d4425aa04db3cefb237169c146f6acce57
Michael Bianco <iloveitaly@gmail.com>
2023-02-25 15:31:06 -0700

    build: adding release script to upload sourcemaps for sentry

---
+++ docker/node-release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+`cd "${0%/*}/.."`
+
+export SENTRY_ORG=knolbe
+export SENTRY_PROJECT=javascript-remix
+# export SENTRY_AUTH_TOKEN=
+
+npx sentry-upload-sourcemaps
+
+# TODO submit updated remix docs for this; the docs were incorrect!
+rm ./public/**/*.map
+rm ./build/*.map
