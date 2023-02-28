677ad87414487c2142b36ad8f782ce2afe2df073
Michael Bianco <iloveitaly@gmail.com>
2023-02-28 05:39:13 -0700

    build: automatically run migrations on node deploy

--- docker/node-release.sh
+++ docker/node-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eux
-`cd "${0%/*}/.."`
+cd "${0%/*}/.."
 
 export SENTRY_ORG=knolbe
 export SENTRY_PROJECT=javascript-remix
@@ -12,3 +12,6 @@ npx sentry-upload-sourcemaps
 # TODO submit updated remix docs for this; the docs were incorrect!
 rm ./public/**/*.map
 rm ./build/*.map
+
+# run migrations on deploy, revert the deploy if something goes wrong
+prisma migrate deploy
