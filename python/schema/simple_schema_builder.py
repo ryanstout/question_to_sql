fdd5a4b8805f519003234b7476d75dab927adf9c
Michael Bianco <iloveitaly@gmail.com>
2023-02-27 19:48:49 -0700

    refactor: simple schema builder
    this seems to be causing prod issues, refactored for easier debugging

--- python/schema/simple_schema_builder.py
+++ python/schema/simple_schema_builder.py
@@ -2,37 +2,30 @@
 The SqlInspector takes a simple version of the schema consisting of a dict of lists of strings (SimpleSchema type)
 
 This is needed to resolve the table and column names.
-
-
 """
 
+import python.utils.db
 from python.sql.types import SimpleSchema
 from python.utils.sql import unqualified_table_name
 
-from prisma import Prisma
+db = python.utils.db.application_database_connection()
 
 
-class SimpleSchemaBuilder:
-    def __init__(self):
-        self.schema: SimpleSchema = {}
+def build_simple_schema(data_source_id: int) -> SimpleSchema:
+    """
+    Generate the SimpleSchema for a given data source id. Assumes data source ID is valid
+    """
 
-    def build(self, db: Prisma, data_source_id: int) -> SimpleSchema:
-        """
-        Generate the SimpleSchema for a given data source id
-        """
+    schema: SimpleSchema = {}
 
-        data_source = db.datasource.find_first(where={"id": data_source_id})
+    for table in db.datasourcetabledescription.find_many(where={"dataSourceId": data_source_id}):
+        fqn = table.fullyQualifiedName
+        table_name = unqualified_table_name(fqn)
+        lower_table_name = table_name.lower()
 
-        if not data_source:
-            raise ValueError(f"Data Source {data_source_id} not found")
+        schema[lower_table_name] = {"name": table_name, "columns": {}}
 
-        for table in db.datasourcetabledescription.find_many(where={"dataSourceId": data_source_id}):
-            fqn = table.fullyQualifiedName
-            table_name = unqualified_table_name(fqn)
-            lower_table_name = table_name.lower()
-            self.schema[lower_table_name] = {"name": table_name, "columns": {}}
+        for column in db.datasourcetablecolumn.find_many(where={"dataSourceTableDescriptionId": table.id}):
+            schema[lower_table_name]["columns"][column.name.lower()] = {"name": column.name}
 
-            for column in db.datasourcetablecolumn.find_many(where={"dataSourceTableDescriptionId": table.id}):
-                self.schema[lower_table_name]["columns"][column.name.lower()] = {"name": column.name}
-
-        return self.schema
+    return schema
