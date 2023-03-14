f8ab8a38d49ca869e9d36ffed44366d35e1510fa
Michael Bianco <iloveitaly@gmail.com>
2023-03-14 06:56:55 -0600

    docs: more context on date error

--- python/tests/sql/test_dates.py
+++ python/tests/sql/test_dates.py
@@ -1,5 +1,9 @@
+# error is on the snowflake compilation side, we need to adjust the SQL dialect stuff
+# https://knolbe.sentry.io/issues/3991381422/?project=4504730283606016&query=is%3Aunresolved&referrer=issue-stream
 def test_now():
-    query = """
+    # `CURRENT_TIMESTAMP() - INTERVAL '1 month'` should be used instead
+    unused_question = "how many text messages did we send last month?"
+    unused_query = """
   SELECT
     COUNT(*) AS number_of_text_messages
   FROM text_messages
